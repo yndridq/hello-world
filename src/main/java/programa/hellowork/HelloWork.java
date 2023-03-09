5860d3d1a38f6d1626dd7c5db0052e7bfe417de8
yndridq <yndrid346h@gmil.com>
2023-03-09 01:11:44 +0000

    three commit

--- src/main/java/programa/hellowork/HelloWork.java
+++ src/main/java/programa/hellowork/HelloWork.java
@@ -7,5 +7,6 @@ public class HelloWork {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Welcome Yndrid");
+        System.out.println("I LOVE HUGO");
     }
 }
